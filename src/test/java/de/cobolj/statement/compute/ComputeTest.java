a7701860b466744d61c81fb332eb7701807f0d42
flaechsig <hans@flaechsig.de>
2018-10-28 14:17:29 +0100

    Weitere Testfälle

--- src/test/java/de/cobolj/statement/compute/ComputeTest.java
+++ src/test/java/de/cobolj/statement/compute/ComputeTest.java
@@ -13,7 +13,7 @@ import de.cobolj.CobolBaseTest;
  *
  */
 public class ComputeTest extends CobolBaseTest {
-	private static int ANZAHL_TESTFAELLE = 10;
+	private static int ANZAHL_TESTFAELLE = 12;
 
 	 /** 
 	 *@return Liste aller Test-Objekte
