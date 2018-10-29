3e21d0f01d4e968808b1e57fb07e85c3f3f8cc44
flaechsig <hans@flaechsig.de>
2018-10-29 21:52:09 +0100

    Compile-Fehler korrigiert

--- src/main/java/de/cobolj/nodes/RelationalOperatorNode.java
+++ src/main/java/de/cobolj/nodes/RelationalOperatorNode.java
@@ -1,10 +1,7 @@
-package de.cobolj.parser.condition;
+package de.cobolj.nodes;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
-
-import de.cobolj.nodes.ArithmeticNode;
-import de.cobolj.nodes.ConditionNode;
 
 /**
  * 
